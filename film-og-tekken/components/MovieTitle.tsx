43ccfc86eb85312fb97a3750cd4b62553e444def
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-05-01 23:18:04 +0200

    added movietitle component

---
+++ film-og-tekken/components/MovieTitle.tsx
@@ -0,0 +1,39 @@
+//create movietitle component
+
+import React from "react";
+//import info icon
+import { FaInfoCircle } from "react-icons/fa";
+
+function MovieTitle() {
+  return (
+    <div className="flex flex-col p-24 mt-44">
+      <h1 className="text-9xl py-8 font-bold text-white  cursor-pointer whitespace-nowrap">
+        Parasite
+      </h1>
+      <h2 className="text-4xl py-2 font-bold text-white  cursor-pointer whitespace-nowrap">
+        Samlet rating: 7.9
+      </h2>
+      <button
+        className="
+        //create grayish background with transparent
+        bg-gray-500 bg-opacity-80
+        hover:bg-gray-700
+        w-52
+        transition duration-300 ease-in-out
+        text-white
+        p-4
+        rounded-lg
+        text-2xl
+        mt-4
+        flex
+        justify-center
+        items-center
+        "
+      >
+        <FaInfoCircle /> <p className="ml-4">Mer info</p>
+      </button>
+    </div>
+  );
+}
+
+export default MovieTitle;
--- film-og-tekken/pages/index.tsx
+++ film-og-tekken/pages/index.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Nav from "@/components/Nav";
 import HomepageImage from "@/components/HomepageImage";
+import MovieTitle from "@/components/MovieTitle";
 
 export default function Home() {
   return (
     <main>
       <HomepageImage>
-
-      <Nav />
+        <Nav />
+        <MovieTitle  />
       </HomepageImage>
-     
     </main>
   );
 }
