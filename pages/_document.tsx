4b8e32086e3050b7150147a4fcb89559e257201e
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-05-04 22:30:53 +0200

    added head title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head  title='Film og Tekken'/>
       <body>
         <div style={{ position: "absolute", top: "0", left: "0", zIndex: "2" }}>
           <Nav />
