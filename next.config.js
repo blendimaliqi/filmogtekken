435404e9bca7f4d6339ea32930847a97d42c1739
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-05-04 21:27:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io", "m.media-amazon.com", "api.themoviedb.org", "image.tmdb.org", "www.themoviedb.org"],
+    domains: [
+      "cdn.sanity.io",
+      "m.media-amazon.com",
+      "api.themoviedb.org",
+      "image.tmdb.org",
+      "www.themoviedb.org",
+    ],
   },
   env: {
     SANITY_PROJECT_ID: process.env.REACT_APP_SANITY_PROJECT_ID,
     SANITY_TOKEN: process.env.REACT_APP_SANITY_TOKEN,
     TMDB_API_KEY: process.env.REACT_APP_TMDB_API_KEY,
   },
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
 
 module.exports = nextConfig;
