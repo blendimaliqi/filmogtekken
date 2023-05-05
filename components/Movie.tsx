ec26a95a63b3dd84cb457770a15000915d9cece3
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-05-05 11:01:35 +0200

    added year and title text when hovering over movies in homepage

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -28,24 +28,36 @@ function Movie({ title, year, poster, movie }: MovieProps) {
           src={urlFor(poster).url()}
           alt="poster"
         />
+
+        <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-opacity duration-200"></div>
+
+        <div className="absolute bottom-0 left-0 w-full bg-opacity-0 text-start p-4 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 transition-opacity duration-200">
+          {movie.title && (
+            <h3 className="text-white font-semibold text-xl">{title}</h3>
+          )}
+          {movie.releaseDate && (
+            <p className="text-white font-light">
+              {movie.releaseDate.split("-")[0]}
+            </p>
+          )}
+        </div>
+
         <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-200">
           <div className="text-center">
             <h3 className="text-white font-semibold text-4xl z-40 group-hover:drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] transition-all duration-200">
-              {
-                movie.ratings && movie.ratings.length > 0 ? (
-                  <div className="flex items-center">
-                    {(
-                      movie.ratings.reduce(
-                        (acc: any, curr: any) => acc + curr.rating,
-                        0
-                      ) / movie.ratings.length
-                    ).toFixed(2)}
-                    <AiFillStar style={{marginTop: "4px"}} size={30} />
-                  </div>
-                ) : (
-                  "Ingen rating"
-                )
-              }
+              {movie.ratings && movie.ratings.length > 0 ? (
+                <div className="flex items-center">
+                  {(
+                    movie.ratings.reduce(
+                      (acc: any, curr: any) => acc + curr.rating,
+                      0
+                    ) / movie.ratings.length
+                  ).toFixed(2)}
+                  <AiFillStar style={{ marginTop: "4px" }} size={30} />
+                </div>
+              ) : (
+                "Ingen rating"
+              )}
             </h3>
           </div>
         </div>
@@ -53,5 +65,6 @@ function Movie({ title, year, poster, movie }: MovieProps) {
     </Link>
   );
 }
+
 
 export default Movie;
