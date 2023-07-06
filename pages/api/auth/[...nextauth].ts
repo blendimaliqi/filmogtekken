7e014affc3d097585443ce108341224337a5d7f2
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-07-06 17:20:11 +0200

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,45 +6,43 @@ import DiscordProvider from "next-auth/providers/discord";
 export const authOptions: AuthOptions = {
   providers: [
     DiscordProvider({
-      clientId: "1119999195206656101",
-      clientSecret: "VSqJA5nlREZiSIcWuyPci2IneLLTYksy",
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
       authorization:
         "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
     }) as Provider,
   ],
   callbacks: {
-    async redirect(params: { url: string; baseUrl: string }) {
-      const { url, baseUrl } = params;
-      if (url.startsWith(baseUrl)) {
-        return url;
-      } else {
-        return `https://filmogtekken-blendimaliqi.vercel.app/api/auth/callback/discord`;
-      }
-    },
-
     async signIn({ account }) {
-      const guildId = "1089621917490761854";
+      if (account?.provider === "discord") {
+        const guildId = process.env.DISCORD_GUILD_ID;
 
-      const response = await fetch(
-        "https://discord.com/api/v10/users/@me/guilds",
-        {
-          headers: {
-            Authorization: `Bearer ${account?.access_token}`,
-          },
-        }
-      );
+        try {
+          const response = await fetch(
+            "https://discord.com/api/v10/users/@me/guilds",
+            {
+              headers: {
+                Authorization: `Bearer ${account?.access_token}`,
+              },
+            }
+          );
 
-      if (response.ok) {
-        const guilds = await response.json();
+          if (response.ok) {
+            const guilds = await response.json();
 
-        const isMember = guilds.some((guild: any) => guild.id === guildId);
+            const isMember = guilds.some((guild: any) => guild.id === guildId);
 
-        if (!isMember) {
-          throw new Error("You are not a member of the desired guild");
-        }
-      } else {
-        console.error("Failed to fetch user guilds. Status:", response.status);
-        throw new Error("Failed to fetch user guilds");
+            if (!isMember) {
+              throw new Error("You are not a member of the desired guild");
+            }
+          } else {
+            console.error(
+              "Failed to fetch user guilds. Status:",
+              response.status
+            );
+            throw new Error("Failed to fetch user guilds");
+          }
+        } catch (error) {}
       }
 
       return Promise.resolve(true);
