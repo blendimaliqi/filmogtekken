a3a99cf8eeb1f2e39fc50b0817e0e366f6088df3
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-07-06 17:29:48 +0200

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { Provider } from "next-auth/providers";
 import { AuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID ?? "",
