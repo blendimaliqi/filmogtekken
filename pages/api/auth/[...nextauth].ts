4e52ab6fbd343358890d766564b536d183e6f202
Blendi Maliqi <blendi_maliqi@outlook.com>
2023-07-07 10:44:47 +0200

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,54 +6,54 @@ import DiscordProvider from "next-auth/providers/discord";
 const authOptions: AuthOptions = {
   providers: [
     DiscordProvider({
-      clientId: "1119999195206656101",
-      clientSecret: "VSqJA5nlREZiSIcWuyPci2IneLLTYksy",
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
       authorization:
         "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
     }) as Provider,
   ],
   callbacks: {
     async signIn({ account }) {
-      // if (account?.provider === "discord") {
-      //   const guildId = process.env.DISCORD_GUILD_ID;
+      if (account?.provider === "discord") {
+        const guildId = process.env.DISCORD_GUILD_ID;
 
-      //   try {
-      //     const response = await fetch(
-      //       "https://discord.com/api/v10/users/@me/guilds",
-      //       {
-      //         headers: {
-      //           Authorization: `Bearer ${account?.access_token}`,
-      //           Redirect: "follow",
-      //         },
-      //       }
-      //     );
+        try {
+          const response = await fetch(
+            "https://discord.com/api/v10/users/@me/guilds",
+            {
+              headers: {
+                Authorization: `Bearer ${account?.access_token}`,
+                Redirect: "follow",
+              },
+            }
+          );
 
-      //     console.log("RESPONSE", response);
+          console.log("RESPONSE", response);
 
-      //     if (response.ok) {
-      //       const guilds = await response.json();
+          if (response.ok) {
+            const guilds = await response.json();
 
-      //       const isMember = guilds.some((guild: any) => guild.id === guildId);
+            const isMember = guilds.some((guild: any) => guild.id === guildId);
 
-      //       if (!isMember) {
-      //         throw new Error("You are not a member of the desired guild");
-      //       }
-      //     } else {
-      //       console.error(
-      //         "Failed to fetch user guilds. Status:",
-      //         response.status
-      //       );
-      //       throw new Error("Failed to fetch user guilds");
-      //     }
-      //   } catch (error) {}
-      // }
+            if (!isMember) {
+              throw new Error("You are not a member of the desired guild");
+            }
+          } else {
+            console.error(
+              "Failed to fetch user guilds. Status:",
+              response.status
+            );
+            throw new Error("Failed to fetch user guilds");
+          }
+        } catch (error) {}
+      }
 
       return Promise.resolve(true);
     },
   },
-  // pages: {
-  //   error: "/not-a-member",
-  // },
+  pages: {
+    error: "/not-a-member",
+  },
 };
 
 export default NextAuth(authOptions);
